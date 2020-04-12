87145e69408aefae245994a11186e4e922917086
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-11 21:17:26 -0700

    initial html render from lit element

--- open-brewery.js
+++ open-brewery.js
@@ -1,4 +1,4 @@
-import { LitElement } from 'https://unpkg.com/lit-element?module';
+import { html, LitElement } from 'https://unpkg.com/lit-element?module';
 
 class OpenBrewery extends LitElement {
   constructor() {
@@ -6,9 +6,15 @@ class OpenBrewery extends LitElement {
   }
 
   connectedCallback() {
+    super.connectedCallback();
     console.log('connected Lit Element to the DOM');
   }
 
+  render() {
+    return html`
+      <h1>My brewery app</h1>
+    `;
+  }
 };
 
 customElements.define('open-brewery', OpenBrewery);
