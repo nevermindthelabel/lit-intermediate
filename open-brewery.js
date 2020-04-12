8f19448eff9223e2e845e8eebf942ffc11fc04c4
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-11 21:41:41 -0700

    added api call and json response

--- open-brewery.js
+++ open-brewery.js
@@ -1,18 +1,31 @@
 import { html, LitElement } from 'https://unpkg.com/lit-element?module';
 
+const api = 'https://api.openbrewerydb.org/breweries';
+
 class OpenBrewery extends LitElement {
-  constructor() {
-    super();
+  static get properties() {
+    return {
+      breweries: { type: Array }
+    }
   }
 
   connectedCallback() {
     super.connectedCallback();
-    console.log('connected Lit Element to the DOM');
+    if(!this.breweries) {
+      this.fetchBrewries();
+    }
+  }
+
+  async fetchBrewries() {
+    let response = await fetch(api);
+    let data = await response.json();
+    this.breweries = data;
   }
 
   render() {
     return html`
       <h1>My brewery app</h1>
+      <pre>${JSON.stringify(this.breweries, null, 2)}</pre>
     `;
   }
 };
