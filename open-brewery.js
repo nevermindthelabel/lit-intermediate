08797c18dfc80aaf15a2eae6169153064eee3a84
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-11 21:14:03 -0700

    added initial methods

--- open-brewery.js
+++ open-brewery.js
@@ -1,6 +1,13 @@
 import { LitElement } from 'https://unpkg.com/lit-element?module';
 
 class OpenBrewery extends LitElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    console.log('connected Lit Element to the DOM');
+  }
 
 };
 
