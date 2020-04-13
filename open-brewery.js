bc973a828f6a64d38db02fbf56764793ef6bfb18
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-12 21:44:38 -0700

    updated formatting

--- open-brewery.js
+++ open-brewery.js
@@ -7,13 +7,13 @@ class OpenBrewery extends LitElement {
   static get properties() {
     return {
       breweries: { type: Array },
-      loading: { type: Boolean}
+      loading: { type: Boolean }
     }
   }
 
   connectedCallback() {
     super.connectedCallback();
-    if(!this.breweries) {
+    if (!this.breweries) {
       this.fetchBrewries();
     }
   }
@@ -27,8 +27,8 @@ class OpenBrewery extends LitElement {
   }
 
   render() {
-    if(this.loading) {
-        return html`
+    if (this.loading) {
+      return html`
         <p>Please wait while the data is fetched...</p>
       `;
     }
@@ -36,11 +36,15 @@ class OpenBrewery extends LitElement {
     return html`
       <h1>My brewery app</h1>
       <pre>${JSON.stringify(this.breweries, null, 2)}</pre>
-      ${this.breweries.map(brewery => html`<brewery-detail
-      .breweryName=${brewery.name}
-      .breweryCity=${brewery.city}
-      .breweryType=${brewery.brewery_type}
-      ></brewery-detail>`)
+      ${this.breweries.map(brewery => html`
+        <li>
+          <brewery-detail
+          .breweryName=${brewery.name}
+          .breweryCity=${brewery.city}
+          .breweryType=${brewery.brewery_type}
+          ></brewery-detail>
+        </li>
+      `)
       }
     `;
   }
